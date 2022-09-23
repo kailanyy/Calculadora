44a727d40b983c747dd8373e1a684d0f4c667b39
KAILANY IARAMENCO SOUZA <kailany_i_souza@estudante.sesisenai.org.br>
2022-09-23 17:15:38 -0300

    fim calculadora

--- Calculadora/calc.js
+++ Calculadora/calc.js
@@ -1,24 +1,29 @@
-var a = Math.floor(Math.random() * 5) + 1;
-var b = Math.floor(Math.random() * 5) + 1;
-var operation = ["+", "-"][Math.floor(Math.random()*2)];
+do{
+    var a = Math.floor(Math.random() * 5) + 1;
+    var b = Math.floor(Math.random() * 5) + 1;
+    var sinal = ["+", "-"][Math.floor(Math.random()*2)];
+    var resposta = eval(a + sinal + b)
+
+}while(resposta<0 || resposta>9)
 
 document.getElementById('n1').innerHTML = a
 document.getElementById('n2').innerHTML = b
-document.getElementById('operation').innerHTML = operation
+document.getElementById('sinal').innerHTML = sinal
 
+document.onkeyup = function (evento){
+    let tecla = Number(evento.key)
 
-function ask() {
-    var answer = document.getElementById('answer').value
+    document.getElementById('answer').value = tecla
 
-    if(answer == ''){
-        document.getElementById('p').innerHTML = "Por favor, digite uma resposta"
-        document.getElementById('img').src = "https://static.wikia.nocookie.net/lifeinthedreamhouse/images/8/88/361.gif/revision/latest/scale-to-width-down/250?cb=20171030194352"
-    } else if (answer == eval(a + operation + b)) {
-        document.getElementById('p').innerHTML = "Você acertou!"
-        document.getElementById('img').src = "https://thumbs.gfycat.com/WhisperedRipeAltiplanochinchillamouse-size_restricted.gif"
-    } else if (answer != eval(a + operation + b)){
-        document.getElementById('p').innerHTML = "Você errou!\nA resposta correta é: " + eval(a + operation + b)
-        document.getElementById('img').src = "https://i.pinimg.com/originals/a9/63/ec/a963ec81a4aa84c1df9529c3a83150f0.gif"
-        document.getElementById('button2').innerHTML = `<button onclick="window.location.reload()" id="newQuestion">Recarregar pergunta</button>`
+    setTimeout(function() {
+        window.location.reload(1);
+    }, 2500);
+
+    if(tecla == eval(a + sinal + b)){
+        document.getElementById('p').innerHTML = "Uau, você é muito inteligente!"
+        document.getElementById('img').src = 'https://thumbs.gfycat.com/WhisperedRipeAltiplanochinchillamouse-size_restricted.gif'
+    } else {
+        document.getElementById('p').innerHTML = `A resposta está errada, a resposta correta é ${resposta}`
+        document.getElementById('img').src = 'https://i.pinimg.com/originals/a9/63/ec/a963ec81a4aa84c1df9529c3a83150f0.gif'
     }
 }
